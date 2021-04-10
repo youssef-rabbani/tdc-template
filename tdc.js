83c58921ebe37e9c0c05571a85e9601e0975caff
youssef-rabbani <42802498+youssef-rabbani@users.noreply.github.com>
2021-04-10 14:33:15 +0300

    initial commit

---
+++ tdc.js
@@ -0,0 +1,52 @@
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+//to avoid errors in workbench: you can remove this when you have added an app
+var app;
+require.config({
+	baseUrl: (config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "" ) + config.prefix + "resources"
+});
+
+require(["js/qlik"], function (qlik) {
+	window.qlik = qlik;
+	
+	qlik.setOnError( function (error) {
+		console.log(error);
+	});
+
+	
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	var app = qlik.openApp('Tourism.qvf', config);
+
+	var app1 = qlik.openApp('test app.qvf', config);
+
+
+	//get objects -- inserted here --
+	app1.getObject('QV-19','cEqNznQ');
+	app1.getObject('QV-14','jKVfuek');
+
+	
+	
+	
+	//create cubes and lists -- inserted here --
+
+
+	if (app) {
+		app.getObject('CurrentSelections','CurrentSelections');
+		$(".filter-drawer-toggle, paper-menu paper-item").click(function() {
+			qlik.resize();
+		});
+	} else {
+		$(".current-selections-placeholder span").css("display", "inline-block");
+	}
+});

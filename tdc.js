a37d5a4d9cadb80631c93c33c77d6ad5a1cbcdd6
youssef-rabbani <42802498+youssef-rabbani@users.noreply.github.com>
2021-04-12 13:59:32 +0300

    spacing fix

--- tdc.js
+++ tdc.js
@@ -32,44 +32,31 @@ require(["js/qlik"], function (qlik) {
 
 	var app2 = qlik.openApp('Overview-2.qvf', config);
 
+	var app3 = qlik.openApp('Overview-3.qvf', config);
+
 
 	//get objects -- inserted here --
-	app2.getObject('QV-81','cHGj');
-	app2.getObject('QV-101','nKngt');
-	app2.getObject('QV-91','nKngt');
+	app3.getObject('QV-23','mfpJhMV');
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	app2.getObject('QV-17','7d756d79-54ca-42ff-a0b4-bd3f526b4e73');
-	
-	
-	
-	
-	app2.getObject('QV-4','TEPLnq');
-	app2.getObject('QV-3','QAhDGR');
-	app2.getObject('QV-2','TEPLnq');
-	app2.getObject('QV-1','BHFqp');
-	
-	
-	app1.getObject('QV-34','jKVfuek');
-	
-	
-	
-	
-	app1.getObject('QV-19','cEqNznQ');
+	app3.getObject('QV-13','NPetkF');
+	app3.getObject('QV-11','DhmLK');
+	app3.getObject('QV-16','a44db455-c169-47f9-9a69-d6ad85aadf8c');
+	app3.getObject('QV-15','92cc4669-e45d-47f0-9b7d-859ecee6e34a');
+	app3.getObject('QV-21','7d756d79-54ca-42ff-a0b4-bd3f526b4e73');
+	app3.getObject('QV-22','24fb6fe4-acd8-437f-a0ad-0f88ba7c7990');
+	app3.getObject('QV-6','zGLYr');
+	app3.getObject('QV-5','mFYgHxD');
+	app3.getObject('QV-4','PAMzpN');
+	app3.getObject('QV-3','TEPLnq');
+	app3.getObject('QV-2','TEPLnq');
+	app3.getObject('QV-1','BHFqp');
+	app3.getObject('QV-14','a834aeab-db64-4cd1-bea6-9ba52e874343');
+	app3.getObject('QV-12','99551a4a-7491-4699-9984-fcb482208386');
+	app3.getObject('QV-10','83a62706-9715-4fab-bb64-698a8a3c959f');
+	app3.getObject('QV-9','a0661c79-9de9-4270-be20-eb32e45a3369');
+	app3.getObject('QV-7','f15c5f34-d89d-4582-8c5d-6ab0e7fa5503');
+	app3.getObject('QV-8','af73e3e3-f879-444d-bbac-09d093c4f92c');
+
 	
 
 	
